cec37a49b6e3bc3fdb982bbba8ba8bcd6edc9d17
DEEPASHIKA <deepashika.padmathilaka@gmail.com>
2016-05-08 13:53:39 +0530

    Initial Files Added

---
+++ AdvancedSoftwareAssignment/src/sanchez016/IOSpecialist.java
@@ -0,0 +1,14 @@
+package sanchez016;
+
+/**
+ * @author Kevan Buckley, maintained by Julio Sanchez
+ * @version 2.0, 2014
+ */
+
+public class IOSpecialist {
+  public IOSpecialist() {   
+  }
+  public String getString(){
+    return IOLibrary.getString(); // middle man
+  }
+}
---
+++ AdvancedSoftwareAssignment/src/sanchez016/Kettering.java
@@ -0,0 +1,16 @@
+package sanchez016;
+
+// shortgun
+public class Kettering {
+  static transient String[] parrot = {
+    "One of the striking differences between a cat and a lie is that a cat has only nine lives.","Mark Twain","",
+    "Arise and take our stand for freedom as in the olden time.","Winston Churchill","",
+    "It is better to be beautiful than to be good. But it is better to be good than to be ugly.","Oscar Wilde","",
+    "Every great advance in science has issued from a new audacity of the imagination.","John Dewey","",
+    "Great truths can only be forgotten and can never be falsified.","GK Chesterton","",
+    "Lies are usually caused by an undue fear of men.","Nachman of Bratslav","",
+    "It is error alone which needs the support of government. Truth can stand by itself.","Thomas Jefferson","",
+    "Pride is an admission of weakness; it secretly fears all competition and dreads all rivals.","Fulton J. Sheen","",
+    "What is now proved was once only imagined.","William Blake","",
+    "The flowers anew, returning seasons bring! But beauty faded has no second spring.","Frank Moore Colby",""};
+}
